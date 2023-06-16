5f6c0bffbd70a1979c7383b244c1778002b04f3b
Deaca <77636742+HuuTinh2207@users.noreply.github.com>
2023-06-16 16:11:35 -0400

    add declaration

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+/********************************************************************************** 
+ * WEB422 – Assignment 3
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ * 
+ * Name: Huu Tinh Luu Student ID: 152712196 Date: 6/16/2023
+ * Link: 
+ *********************************************************************************/
 import useSWR from 'swr'
 import { useState,useEffect } from 'react'
 import { Pagination,Accordion } from 'react-bootstrap'
